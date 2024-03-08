167ff2e46821701d50293b917a495cd7a1d48bf1
Tom ROBERTO <tom.roberto31@gmail.com>
2024-03-08 11:57:12 +0100

    instaled cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 
+app.use(cors());
 app.use(express.json());
 
 console.log(process.env.MONGODB_URI);
